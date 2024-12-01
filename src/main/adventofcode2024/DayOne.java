f95837d0f089c06b9f2e0ac2ee0e3a132d8f6ad9
Jasper <jasper.syswerda@gmail.com>
2024-12-01 18:45:43 +0100

    DayOne

---
+++ src/main/adventofcode2024/DayOne.java
@@ -0,0 +1,54 @@
+package main.adventofcode2024;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class DayOne extends EveryDay {
+
+    List<Integer> leftList = new ArrayList<>();
+    List<Integer> rightList = new ArrayList<>();
+
+    public static void main(String[] args) {
+        List<String> input = readInputFile("DayOne.txt");
+        DayOne dayOne = new DayOne();
+        dayOne.fillLists(input);
+        //Part1
+        dayOne.calculateSums_Part1();
+        //Part2
+        dayOne.calculateSimilarityScores_Part2();
+
+    }
+
+    private void calculateSimilarityScores_Part2() {
+        int result = 0;
+        for (int number : leftList){
+            int count = (int) rightList.stream().filter(r -> r.equals(number)).count();
+            result += count*number;
+        }
+        System.out.println("Part2: " + result);
+
+    }
+
+    private void calculateSums_Part1() {
+        int result = 0;
+        for (int i = 0; i < leftList.size(); i++){
+            int leftNumber = leftList.get(i);
+            int rightNumber = rightList.get(i);
+            result += Math.abs(leftNumber - rightNumber);
+        }
+        System.out.println("Part1: " + result);
+    }
+
+    private void fillLists(List<String> input) {
+        for (String line : input){
+            String[] numbersAsString = line.split("\\s+");
+            leftList.add(Integer.parseInt(numbersAsString[0]));
+            rightList.add(Integer.parseInt(numbersAsString[1]));
+        }
+        Collections.sort(leftList);
+        Collections.sort(rightList);
+    }
+
+
+}
---
+++ src/main/adventofcode2024/EveryDay.java
@@ -0,0 +1,26 @@
+package main.adventofcode2024;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class EveryDay {
+
+    static List<String> readInputFile(String day) {
+        List<String> list = new ArrayList<>();
+        try {
+            File file = new File("src/main/resources/2024/" + day);
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                list.add(line);
+            }
+            scanner.close();
+            return list;
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
