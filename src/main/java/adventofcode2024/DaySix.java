318aba94fe0128de63bc96f7d728895520eb4527
Jasper <jasper.syswerda@gmail.com>
2024-12-07 11:24:40 +0100

    DaySix Part 1

---
+++ src/main/java/adventofcode2024/DaySix.java
@@ -0,0 +1,71 @@
+package adventofcode2024;
+
+import adventofcode2024.helperDaySix.Coordinate;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class DaySix extends EveryDay{
+    List<String> input;
+    int dRow;
+    int dCol;
+
+    public static void main(String[] args) {
+        var daySix = new DaySix();
+        daySix.input = readInputFile("DaySix.txt");
+        daySix.doPart1();
+    }
+
+    private void doPart1() {
+        Set<Coordinate> positions = new HashSet<>();
+        Coordinate currentPosition = getStartPosition();
+        System.out.println(currentPosition);
+        positions.add(currentPosition);
+        dRow = -1;
+        dCol = 0;
+        while(isInsideBoundaries(currentPosition)){
+            if (getNext(currentPosition) == '#'){
+                rotate90DegRight();
+            }
+            else {
+                currentPosition = new Coordinate(currentPosition.getRow() + dRow, currentPosition.getColumn() + dCol);
+                System.out.println(currentPosition);
+                positions.add(currentPosition);
+            }
+        }
+        System.out.println(positions.stream().distinct().count());
+    }
+
+    private void rotate90DegRight() {
+        int newDRow = dCol;
+        int newDCol = -dRow;
+
+        dRow = newDRow;
+        dCol = newDCol;
+    }
+
+    private char getNext(Coordinate currentPosition) {
+        return input.get(currentPosition.getRow() + dRow).charAt(currentPosition.getColumn() + dCol);
+    }
+
+    private boolean isInsideBoundaries(Coordinate currentPosition) {
+        return currentPosition.getRow() + dRow >= 0
+                && currentPosition.getRow() + dRow < input.size()
+                && currentPosition.getColumn() + dCol >= 0
+                && currentPosition.getColumn() + dCol < input.size();
+    }
+
+    private Coordinate getStartPosition() {
+        for (int i = 0; i < input.size(); i++) {
+            for (int j = 0; j < input.get(0).length(); j++) {
+                if (input.get(i).charAt(j) == '^'){
+                    return new Coordinate(i, j);
+                }
+
+            }
+        }
+        throw new RuntimeException("No start mark found!");
+    }
+
+}
---
+++ src/main/java/adventofcode2024/helperDaySix/Coordinate.java
@@ -0,0 +1,42 @@
+package adventofcode2024.helperDaySix;
+
+import java.util.Objects;
+
+public class Coordinate {
+    int row;
+    int column;
+
+    public Coordinate(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    @Override
+    public String toString() {
+        return "Coordinate{" +
+                "column=" + column +
+                ", row=" + row +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coordinate that = (Coordinate) o;
+        return row == that.row && column == that.column;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, column);
+    }
+}
