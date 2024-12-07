8b29951db1afaaddd1b0b3ad8a151cd8d57e54a7
Jasper <jasper.syswerda@gmail.com>
2024-12-07 11:24:06 +0100

    DayFive Part 2

--- src/main/java/adventofcode2024/DayFive.java
+++ src/main/java/adventofcode2024/DayFive.java
@@ -9,10 +9,43 @@ public class DayFive extends EveryDay{
         List<String> input = readInputFile("DayFive.txt");
         List<String> rules = getRules(input);
         List<String> pagesToProduce = getPagesToProduce(input);
-
         Map<Integer, List<Integer>> orderMap = fillMap(rules);
-        part1(pagesToProduce, orderMap);
 
+        part1(pagesToProduce, orderMap);
+        part2(pagesToProduce, orderMap, rules);
+
+    }
+
+    private static void part2(List<String> pagesToProduce, Map<Integer, List<Integer>> orderMap, List<String> rules) {
+        int result = 0;
+        for (String pagesAsString: pagesToProduce){
+            List<Integer> pageNumbers = getPageNumbers(pagesAsString);
+            if (!isValid(pageNumbers, orderMap)){
+                result += orderListAndFindMiddle(pageNumbers, orderMap, rules);
+            }
+        }
+        System.out.println("Part 2: " + result);
+    }
+
+    private static int orderListAndFindMiddle(List<Integer> pageNumbers, Map<Integer, List<Integer>> orderMap, List<String> rules) {
+        pageNumbers = new ArrayList<>(pageNumbers);
+        while (!isValid(pageNumbers, orderMap)){
+            for (String rule : rules){
+                String[] numbersAsString = rule.split("\\|");
+                int before = Integer.parseInt(numbersAsString[0]);
+                int after = Integer.parseInt(numbersAsString[1]);
+                if (!pageNumbers.contains(before) || !pageNumbers.contains(after)){
+                    continue;
+                }
+                int beforeIndex = pageNumbers.indexOf(before);
+                int afterIndex = pageNumbers.indexOf(after);
+                if (beforeIndex > afterIndex){
+                    pageNumbers.set(beforeIndex, after);
+                    pageNumbers.set(afterIndex, before);
+                }
+            }
+        }
+        return pageNumbers.get(pageNumbers.size()/2);
     }
 
     private static void part1(List<String> pagesToProduce, Map<Integer, List<Integer>> orderMap) {
@@ -20,10 +53,10 @@ public class DayFive extends EveryDay{
         for (String pagesAsString : pagesToProduce){
             List<Integer> pageNumbers = getPageNumbers(pagesAsString);
             if (isValid(pageNumbers, orderMap)){
-                result+=(pageNumbers.get(pageNumbers.size()/2));
+                result += (pageNumbers.get(pageNumbers.size()/2));
             }
         }
-        System.out.println(result);
+        System.out.println("Part 1: " + result);
     }
 
     private static boolean isValid(List<Integer> pageNumbers, Map<Integer, List<Integer>> orderMap) {
